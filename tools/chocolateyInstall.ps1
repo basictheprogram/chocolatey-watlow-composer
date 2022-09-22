4a555528e2865ba3defa86b02990727688b53ff8
Bob Tanner <tanner@real-time.com>
2022-09-22 15:18:09 -0500

    Boilerplate

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+$WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    silentArgs     = '/S'
+    softwareName   = 'Ultimaker'
+    validExitCodes = @(0, 3010, 1641)
+
+    url            = $data.url
+    checksum       = $data.checksum
+    checksumType   = $data.checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/data.ps1
@@ -0,0 +1,5 @@
+@{
+    url = ''
+    checksum = ''
+    checksumType = 'sha256'
+}
