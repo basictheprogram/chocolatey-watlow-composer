2522ad13bd46c35d7753e32b99b8fc57c06e6691
Bob Tanner <tanner@real-time.com>
2022-09-29 12:43:44 -0500

    WIP #1: Watlow COMPOSER device driver certificate
      * Added Import-Certificate cmdlet

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,12 +4,15 @@ $data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -Chil
 $WorkSpace = Join-Path $env:TEMP $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$composer = Join-Path $toolsDir 'Watlow_COMPOSER.cer'
+Import-Certificate -FilePath $composer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'exe'
     silentArgs     = '/S'
-    softwareName   = 'Ultimaker'
+    softwareName   = 'Watlow COMPOSER'
     validExitCodes = @(0, 3010, 1641)
 
     url            = $data.url
